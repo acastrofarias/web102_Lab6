13ee83f72538e591d348c6cd5d51057e0f540cd5
Isaiah Amanuel <isaiahamanuel@gmail.com>
2023-03-29 12:02:02 -0700

    checkpoint 4

--- crypto-hustle-pro/src/main.jsx
+++ crypto-hustle-pro/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./routes/Layout";
 import DetailView from "./routes/DetailView";
+import NotFound from "./routes/NotFound";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             path="/coinDetails/:symbol"
             element={<DetailView />}
           />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ crypto-hustle-pro/src/routes/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const NotFound = () => {
+  return (
+    <main>
+      <p>There's nothing here!</p>
+      <Link to="/">Back to Home</Link>
+    </main>
+  );
+};
+
+export default NotFound;
